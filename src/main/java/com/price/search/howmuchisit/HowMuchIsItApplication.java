54b5fc9606a9240dcf2c5c31530c2ff82f7e3287
cherrue <th885172@gmail.com>
2023-04-02 18:13:02 +0900

    :tada: Initial Commit!

---
+++ src/main/java/com/price/search/howmuchisit/HowMuchIsItApplication.java
@@ -0,0 +1,13 @@
+package com.price.search.howmuchisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowMuchIsItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowMuchIsItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/price/search/howmuchisit/HowMuchIsItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.price.search.howmuchisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowMuchIsItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
